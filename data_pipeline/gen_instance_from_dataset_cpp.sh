8edd6edec0b78994883e7272df3ff70d9707abdc
JasonZhouPW <zhoupw@gmail.com>
2026-03-07 18:42:05 +0800

    update

--- data_pipeline/gen_instance_from_dataset_cpp.sh
+++ data_pipeline/gen_instance_from_dataset_cpp.sh
@@ -150,7 +150,7 @@ ENV LC_ALL=C.UTF-8
 RUN sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list && \\
     sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list && \\
     apt-get update && apt-get install -y libbrotli-dev libcurl4-openssl-dev cmake
-RUN apt-get install -y clang build-essential
+RUN apt-get install -y clang build-essential cmake
 
 {code}
 
